aa3074f19bffdfa430ce4c1a6fabfaa2ef805f4e
yuragms <yura.gms@gmail.com>
2022-08-15 23:15:20 +0300

    server

---
+++ server/Routes/UserRoute.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  getUser,
+  updateUser,
+  deleteUser,
+  followUser,
+  UnfollowUser,
+} from "../Controllers/UserController.js";
+
+const router = express.Router();
+
+// router.get("/", async (req, res) => {
+//   res.send("user route");
+// });
+router.get("/:id", getUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.put("/:id/follow", followUser);
+router.put("/:id/unfollow", UnfollowUser);
+
+export default router;
