fc4038ac96ab43ed401c87c41a867326714cc9f0
yuragms <yura.gms@gmail.com>
2022-08-16 22:04:04 +0300

    corss

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -6,8 +6,13 @@ const authReducer = (
     case "AUTH_START":
       return { ...state, loading: true, error: false };
     case "AUTH_SUCCESS":
+      localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
       return { ...state, authData: action.data, loading: false, error: false };
+    case "AUTH_FAIL":
+      return { ...state, loading: false, error: true };
     default:
       return state;
   }
 };
+
+export default authReducer;
