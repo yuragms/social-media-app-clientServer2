4b6df29bea6ca3db254cc589295c64b5a4442072
yuragms <yura.gms@gmail.com>
2022-08-20 22:00:13 +0300

    unfollow

--- client/src/Components/User/User.jsx
+++ client/src/Components/User/User.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { followUser, unFollowUser } from "../../actions/userAction";
 
 const User = ({ person, id }) => {
+  const dispatch = useDispatch();
+  const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const [following, setFollowing] = useState(
+    person.following.includes(user.id)
+  );
+
+  const handleFollow = () => {
+    following
+      ? dispatch(unFollowUser(person._id, user))
+      : dispatch(followUser(person._id, user));
+    setFollowing((prev) => !prev);
+  };
+
   return (
     <div className="follower" key={id}>
       <div>
@@ -19,7 +34,9 @@ const User = ({ person, id }) => {
           <span>@{person.username}</span>
         </div>
       </div>
-      <button className="button fc-button">Follow</button>
+      <button className="button fc-button" onClick={handleFollow}>
+        {following ? "Unfollow" : "Follow"}
+      </button>
     </div>
   );
 };
--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -87,17 +87,17 @@ export const deleteUser = async (req, res) => {
 export const followUser = async (req, res) => {
   const id = req.params.id;
 
-  const { currentUserId } = req.body;
+  const { _id } = req.body;
 
-  if (id === currentUserId) {
+  if (id === _id) {
     res.status(403).json("Action forbidden");
   } else {
     try {
       const followUser = await UserModel.findById(id);
-      const followingUser = await UserModel.findById(currentUserId);
+      const followingUser = await UserModel.findById(_id);
 
-      if (!followUser.followers.includes(currentUserId)) {
-        await followUser.updateOne({ $push: { followers: currentUserId } });
+      if (!followUser.followers.includes(_id)) {
+        await followUser.updateOne({ $push: { followers: _id } });
         await followingUser.updateOne({ $push: { following: id } });
         res.status(200).json("User followed!");
       } else {
@@ -113,17 +113,17 @@ export const followUser = async (req, res) => {
 export const UnfollowUser = async (req, res) => {
   const id = req.params.id;
 
-  const { currentUserId } = req.body;
+  const { _id } = req.body;
 
-  if (id === currentUserId) {
+  if (id === _id) {
     res.status(403).json("Action forbidden");
   } else {
     try {
       const followUser = await UserModel.findById(id);
-      const followingUser = await UserModel.findById(currentUserId);
+      const followingUser = await UserModel.findById(_id);
 
-      if (followUser.followers.includes(currentUserId)) {
-        await followUser.updateOne({ $pull: { followers: currentUserId } });
+      if (followUser.followers.includes(_id)) {
+        await followUser.updateOne({ $pull: { followers: _id } });
         await followingUser.updateOne({ $pull: { following: id } });
         res.status(200).json("User Unfollowed!");
       } else {
