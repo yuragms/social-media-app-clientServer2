d88bf883b7c31442f26b2742698b6aeb1b948b9e
yuragms <yura.gms@gmail.com>
2022-08-28 11:48:19 +0300

    deb

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -1,4 +1,6 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5001" });
+const API = axios.create({
+  baseURL: "https://git.heroku.com/mongo-socialmedia-app2.git",
+});
 export const logIn = (formData) => API.post("/auth/login", formData);
 export const signUp = (formData) => API.post("/auth/register", formData);
