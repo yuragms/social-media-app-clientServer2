172a16d1fe419166b8fd1534b31d46f4bfae60d6
yuragms <yura.gms@gmail.com>
2022-09-12 15:51:06 +0300

    s31

--- server/Routes/UploadRoute.js
+++ server/Routes/UploadRoute.js
@@ -1,21 +1,18 @@
 import express from "express";
 import multer from "multer";
+import { S3Client } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 router.post("/", upload.single("file"), (req, res) => {
   try {
+    console.log("req.body", req.body);
+    console.log("req.file", req.file);
+    req.file.buffer;
     return res.status(200).json("File Uploaded Successfully");
   } catch (error) {
     console.log(error);
@@ -23,3 +20,27 @@ router.post("/", upload.single("file"), (req, res) => {
 });
 
 export default router;
+
+// import express from "express";
+// import multer from "multer";
+
+// const router = express.Router();
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "public/images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, req.body.name);
+//   },
+// });
+
+// router.post("/", upload.single("file"), (req, res) => {
+//   try {
+//     return res.status(200).json("File Uploaded Successfully");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+// export default router;
